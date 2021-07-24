87d0b2ef61a99dcfb41bd06f17e566217dd268c9
emanuelosva <emanuelosva@gmail.com>
2021-07-23 19:29:18 -0500

    :construction: add config strings

--- config/index.js
+++ config/index.js
@@ -1,5 +1,10 @@
 const config = {
   BASE_URL: 'https://emanuelosva.vercel.app',
+  SITE_TITLE: 'Blog - Many Osorio',
+  SITE_NAME: 'Many Osorio',
+  /** @TODO create og and seo description */
+  SEO_DESCRIPTION: 'Programming, nodejs, backend, frontend',
+  BLOG_DESCRIPTION: 'Opinion, tutorials and discussions about software development.',
 }
 
 export default config
