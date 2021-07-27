86b09dc871ac53e60c23c654496d776466e4802b
emanuelosva <emanuelosva@gmail.com>
2021-07-27 18:05:07 -0500

    :sparkles: hook to fetch related articles

---
+++ hooks/useRelatedBlogs.js
@@ -0,0 +1,17 @@
+import useSWR from 'swr'
+import fetcher from '@/lib/fetcher'
+
+export default function useRelatedBlogs(slug, tags) {
+  const tagsQuery = tags.join(',')
+
+  const { data, error } = useSWR(`/api/blogs?tags=${tagsQuery}&limit=4`, {
+      fetcher,
+      initialData: { blogs: null },
+  })
+
+  return {
+    relatedArticles: data?.blogs?.filter((blog) => blog.slug !== slug),
+    isLoading: !error && !data?.blogs,
+    error,
+  }
+}
