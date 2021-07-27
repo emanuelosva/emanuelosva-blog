1586b218be49cdd343682470c4e5d9aed92cf943
emanuelosva <emanuelosva@gmail.com>
2021-07-27 18:06:57 -0500

    :sparkles: add list of related blogs component

---
+++ components/blogRelateds/index.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import Link from 'next/link'
+import PropTypes from 'prop-types'
+import BlogImage from '@/components/blogImage'
+import Container from '@/components/container'
+import { BlogLinkSkeleton } from '@/components/blogSkeleton'
+import useViews from '@/hooks/useViews'
+import formatDateToString from '@/lib/formatDateToString'
+import { BlogRelatedsContainer } from './styles'
+
+export default function BlogRelateds({ blogs, isLoading }) {
+  const renderLoadingSkeleton = () => (
+    <>
+      <BlogLinkSkeleton />
+      <br />
+      <BlogLinkSkeleton />
+    </>
+  )
+
+  const renderRelatedBlogs = () => blogs?.map((blog) => (
+    <RelatedBlog key={blog.slug} blog={blog} />
+  ))
+
+  return (
+    <BlogRelatedsContainer>
+    <h3>Ralated articles</h3>
+      {
+        isLoading
+          ? renderLoadingSkeleton()
+          : renderRelatedBlogs()
+      }
+    </BlogRelatedsContainer>
+  )
+}
+
+function RelatedBlog({ blog }) {
+  const { views } = useViews(blog.slug)
+
+  return (
+    <Link key={blog.slug} href={`/blog/${blog.slug}`} passHref>
+      <a className='blog-container'>
+        <div className='main-container'>
+          <div className='title-container'>
+            <h4>{blog.title}</h4>
+            <div className='tags-container'>
+              {blog.tags?.map((tag) => <span key={tag}>{tag}</span>)}
+            </div>
+          </div>
+          <div className='meta-container'>
+            <p className='author'>{blog.author.name}</p>
+            <p className='meta'>{formatDateToString(blog.publishedAt)} • {views} views</p>
+          </div>
+        </div>
+        <BlogImage
+          src={blog.image.source}
+          alt={blog.image.description}
+          width='120px'
+          height='120px'
+        />
+      </a>
+    </Link>
+  )
+}
+
+BlogRelateds.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  blogs: PropTypes.arrayOf(
+    PropTypes.shape({
+      slug: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      image: PropTypes.shape({
+        source: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+      }).isRequired,
+      author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string,
+      }),
+      tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+      publishedAt: PropTypes.string,
+    })
+  ),
+}
---
+++ components/blogRelateds/styles.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+
+export const BlogRelatedsContainer = styled.section`
+  width: 100%;
+  height: auto;
+  margin: 1rem auto 2rem;
+  padding: 0 5px;
+
+  h3 {
+    width: fit-content;
+    padding-bottom: 0.5rem;
+    margin-bottom: 1.5rem;
+    border-bottom: 1px solid var(--c-gray-divider);
+  }
+
+  a {
+    cursor: pointer;
+  }
+
+  .blog-container {
+    height: 160px;
+    margin: 2rem 0;
+    display: grid;
+    grid-template-columns: 1.5fr 1fr;
+    /* @TODO verify design */
+    /* border-top: 1px solid var(--c-gray-divider);
+    border-bottom: 1px solid var(--c-gray-divider); */
+  }
+
+  .main-container {
+    display: grid;
+    grid-template-rows: 1.5fr 1fr;
+  }
+
+  .blog-container img {
+    height: 120px;
+    width: 120px;
+  }
+
+  .blog-container h4 {
+    margin: 0 auto 0.5rem;
+  }
+
+  .blog-container .tags-container {
+    display: flex;
+    flex-wrap: wrap;
+    padding-right: 1rem;
+  }
+
+  .blog-container .tags-container span {
+    margin-right: 4px;
+    padding: 2px 4px;
+    color: var(--c-many-orange);
+    background-color: var(--c-gray-divider);
+    border-radius: 4px;
+  }
+
+  .blog-container .meta-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+
+  .blog-container .meta-container p {
+    margin: 0;
+  }
+
+  .blog-container .meta-container .author {
+    font-weight: 600;
+  }
+
+  @media(max-width: 420px) {
+    .blog-container {
+      height: 120px;
+    }
+
+    .blog-container img {
+      height: 80px;
+      width: 80px;
+    }
+
+    .blog-container .meta-container p {
+      font-size: 0.8rem;
+    }
+  }
+`
