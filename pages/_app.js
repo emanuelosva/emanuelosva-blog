739d7825a4f542786577529543f4754f457083d9
emanuelosva <emanuelosva@gmail.com>
2021-07-30 11:46:06 -0500

    :hammer: store theme in localStorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
 import Layout from '@/components/layout'
 import { theme, THEMES, GlobalStyles } from '@/theme'
 import '@/styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
+  const THEME_STORAGE = 'emanuelosva.io-site-theme'
   const [themeName, setTheme] = useState(THEMES.LIGTH)
-  const toogleTheme = () => setTheme(themeName === THEMES.LIGTH ? THEMES.DARK : THEMES.LIGTH)
+
+  const toogleTheme = () => {
+    const toggledTheme = themeName === THEMES.LIGTH ? THEMES.DARK : THEMES.LIGTH
+    setTheme(toggledTheme)
+    window?.localStorage.setItem(THEME_STORAGE, toggledTheme)
+  }
+
+  useEffect(() => {
+    const actualTheme = window?.localStorage.getItem(THEME_STORAGE)
+    actualTheme && setTheme(actualTheme)
+  }, [])
 
   return (
     <ThemeProvider theme={theme[themeName]}>
