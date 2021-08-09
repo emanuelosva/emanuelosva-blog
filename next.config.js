d92762675c9362193b110a896e65ea2a39b86436
emanuelosva <emanuelosva@gmail.com>
2021-08-09 15:04:16 -0500

    :hammer: add security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+]
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['emanuelosva-blog-images.s3.amazonaws.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
